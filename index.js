1018c595a1a3730d16db422d2827e3061a9303d7
filecc <fil.gia@live.it>
2023-05-08 15:41:58 +0200

    added mark completed on click

--- index.js
+++ index.js
@@ -1,38 +1,66 @@
-const { createApp } = Vue
+const { createApp } = Vue;
 
-  createApp({
-    data() {
-      return {
-        todos: null,
-        apiUrl: './server.php',
-        newTask: ''
+createApp({
+  data() {
+    return {
+      todos: null,
+      apiUrl: "./server.php",
+      newTask: "",
+    };
+  },
+  methods: {
+    getData() {
+      axios.get(this.apiUrl).then((res) => {
+        this.todos = res.data;
+        // ordering
+        this.todos.sort((a, b) => {
+          if (a.completed === "0" && b.completed === "1") {
+            return -1;
+          }
+          if (a.completed === "1" && b.completed === "0") {
+            return 1;
+          }
+          return 0;
+        });
+      });
+    },
+    addTask() {
+      const data = {
+        content: this.newTask,
+      };
+
+      axios
+        .post(this.apiUrl, data, {
+          headers: {
+            "Content-Type": "multipart/form-data",
+          },
+        })
+        .then((res) => {
+          this.newTask = "";
+          this.getData();
+        });
+    },
+    removeTask(e) {
+      id = e.currentTarget.id;
+      const found = this.todos.find((element) => element.id === id);
+      if (found.completed === "1") {
+        found.completed = "0";
+      } else {
+        found.completed = "1";
       }
-    },
-    methods: {
-        getData(){
-            axios.get(this.apiUrl).then((res) => {
-               this.todos = res.data;
-            })
-        },
-        addTask(){
-            console.log(this.newTask)
-            const data = {
-                content: this.newTask,
-                completed: false
-            }
 
-            axios.post(this.apiUrl, data, {headers: {
-                'Content-Type': 'mmultipart/form-data'
-              }}).then((res) => {
-                this.newTask = '';
-                this.getData();
-              });
-        },
-        removeTask(){
-            
-        }
+      axios
+        .post(this.apiUrl, this.todos, {
+          headers: {
+            "Content-Type": "multipart/form-data",
+          },
+        })
+        .then((res) => {
+          this.getData();
+        });
     },
-    mounted(){
-        this.getData();
-    }
-  }).mount('#app')
+  },
+  mounted() {
+    this.getData();
+  },
+}).mount("#app");
--- server.php
+++ server.php
@@ -1,18 +1,27 @@
 <?php 
-
-$file = file_get_contents('data.json');
-$file_decoded = json_decode($file, true);
-
-
+ $file = file_get_contents('data.json');
+ $file_decoded = json_decode($file, true);
+ 
 if (!empty($_POST['content'])){
+    $randomID = chr(rand(97,122)) . rand(10,99) . chr(rand(97,122));
     $newTask = array(
         "content" => $_POST['content'],
-        "completed" => false
+        "completed" => '0',
+        "id" => $randomID
     );
     array_push($file_decoded, $newTask);
     file_put_contents('data.json', json_encode($file_decoded));
-}
-
-
+} else {
+    if(!empty($_POST) && empty($_POST['content'])){
+        $data = ($_POST);
+        file_put_contents('data.json', json_encode($data));
+    }
+   
+} 
 
 echo json_encode($file_decoded);
+
+
+
+
+
